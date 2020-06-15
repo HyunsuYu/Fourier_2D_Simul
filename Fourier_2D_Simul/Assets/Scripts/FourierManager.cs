ba744cb20d01acc7a466dfbeb5c91bc1a0293495
HyunsuYu <63137431+HyunsuYu@users.noreply.github.com>
2020-06-16 03:25:46 +0900

    Clear

--- Fourier_2D_Simul/Assets/Scripts/FourierManager.cs
+++ Fourier_2D_Simul/Assets/Scripts/FourierManager.cs
@@ -10,16 +10,24 @@ public class FourierManager : MonoBehaviour
     public GameObject node;
     public GameObject dot;
 
-    private GameObject temp;
+    private float timer;
+    private int counter = 0;
+    private int repeatNum = 10;
 
     Fourier_2D_SimulDll.Fourier_2D_Simul fourier;
 
     void Start()
     {
-        Fourier_2D_Simul fourier = new Fourier_2D_Simul(new Fourier_2D_Simul.InputPack(0, 0, 5, 5, 5, 0.01, circle, vector, node, dot));
-        InvokeRepeating("Temp", 0.0f, 1.0f);
+        fourier = new Fourier_2D_Simul(new Fourier_2D_Simul.InputPack(25, 25, 15, 5, 5, 0.001, circle, vector, node, dot));
+        //for(int i = 0; i < 1000; i++)
+        //{
+        //    fourier.MakeFourier();
+        //}
+
+        InvokeRepeating("temp", 0.0f, 0.1f);
     }
-    private void Temp()
+
+    private void temp()
     {
         fourier.MakeFourier();
     }
